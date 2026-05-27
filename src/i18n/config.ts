24002648e957f27bdae7346ee43fe89af437db66
Martin Roncero <mroncero@indra.es>
2026-05-27 11:30:40 +0200

    feats & fixes(lang): support to more languages

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,19 +1,29 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import en from './locales/en.json';
-import es from './locales/es.json';
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next" // localStorage.removeItem('i18nextLng')
+import LanguageDetector from "i18next-browser-languagedetector"
+import en from "./locales/en.json"
+import es from "./locales/es.json"
+import ar from "./locales/ar.json"
+import fr from "./locales/fr.json"
+import hi from "./locales/hi.json"
+import pt from "./locales/pt.json"
+import zh from "./locales/zh.json"
 
 i18n
-    .use(LanguageDetector) // Automatically detects user browser language
-    .use(initReactI18next)
-    .init({
-        resources: {
-            en: { translation: en },
-            es: { translation: es }
-        },
-        fallbackLng: 'en', // Use English if a Spanish translation is missing
-        interpolation: { escapeValue: false } // React already protects against XSS
-    });
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+      ar: { translation: ar },
+      fr: { translation: fr },
+      hi: { translation: hi },
+      pt: { translation: pt },
+      zh: { translation: zh },
+    },
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  })
 
-export default i18n;
+export default i18n
